b1f1593406240acf7cb4a46f080437e25f17e3e4
shaodushu <shaodushu@gmail.com>
2022-06-07 22:57:58 +0800

    test:测试

--- index.js
+++ index.js
@@ -20,67 +20,67 @@ http
   .createServer(async (req, res) => {
     console.log("receive request");
     console.log(req.url);
-    if (req.method === "POST" && req.url === "/") {
-      const data = await resolvePost(req);
-      const projectDir = path.resolve(__dirname, `./${data.repository.name}`);
-      deleteFolderRecursive(projectDir);
+    // if (req.method === "POST" && req.url === "/") {
+    //   const data = await resolvePost(req);
+    //   const projectDir = path.resolve(__dirname, `./${data.repository.name}`);
+    //   deleteFolderRecursive(projectDir);
 
-      execSync(
-        `git clone https://github.com/shaodushu/FreeWater.git ${projectDir}`,
-        {
-          stdio: "inherit",
-        }
-      );
-      // 拉取仓库最新代码
-      // execSync(
-      //   `git clone https://github.com/yeyan1996/${data.repository.name}.git ${projectDir}`,
-      //   {
-      //     stdio: "inherit",
-      //   }
-      // );
+    //   execSync(
+    //     `git clone https://github.com/shaodushu/FreeWater.git ${projectDir}`,
+    //     {
+    //       stdio: "inherit",
+    //     }
+    //   );
+    //   // 拉取仓库最新代码
+    //   // execSync(
+    //   //   `git clone https://github.com/yeyan1996/${data.repository.name}.git ${projectDir}`,
+    //   //   {
+    //   //     stdio: "inherit",
+    //   //   }
+    //   // );
 
-      // 复制 Dockerfile 到项目目录
-      fs.copyFileSync(
-        path.resolve(__dirname, `./Dockerfile`),
-        path.resolve(projectDir, "./Dockerfile")
-      );
+    //   // 复制 Dockerfile 到项目目录
+    //   fs.copyFileSync(
+    //     path.resolve(__dirname, `./Dockerfile`),
+    //     path.resolve(projectDir, "./Dockerfile")
+    //   );
 
-      // 复制 .dockerignore 到项目目录
-      fs.copyFileSync(
-        path.resolve(__dirname, `./.dockerignore`),
-        path.resolve(projectDir, "./.dockerignore")
-      );
+    //   // 复制 .dockerignore 到项目目录
+    //   fs.copyFileSync(
+    //     path.resolve(__dirname, `./.dockerignore`),
+    //     path.resolve(projectDir, "./.dockerignore")
+    //   );
 
-      // 创建 docker 镜像
-      execSync(`docker build -t ${data.repository.name}-image:latest .`, {
-        stdio: "inherit",
-        cwd: projectDir,
-      });
+    //   // 创建 docker 镜像
+    //   execSync(`docker build -t ${data.repository.name}-image:latest .`, {
+    //     stdio: "inherit",
+    //     cwd: projectDir,
+    //   });
 
-      // // 拉取 docker 镜像
-      // execSync(`docker pull yeyan1996/docker-test-image:latest`, {
-      //     stdio: 'inherit',
-      //     cwd: projectDir
-      // })
+    //   // // 拉取 docker 镜像
+    //   // execSync(`docker pull yeyan1996/docker-test-image:latest`, {
+    //   //     stdio: 'inherit',
+    //   //     cwd: projectDir
+    //   // })
 
-      // 销毁 docker 容器
-      execSync(
-        `docker ps -a -f "name=^${data.repository.name}-container" --format="{{.Names}}" | xargs -r docker stop | xargs -r docker rm`,
-        {
-          stdio: "inherit",
-        }
-      );
+    //   // 销毁 docker 容器
+    //   execSync(
+    //     `docker ps -a -f "name=^${data.repository.name}-container" --format="{{.Names}}" | xargs -r docker stop | xargs -r docker rm`,
+    //     {
+    //       stdio: "inherit",
+    //     }
+    //   );
 
-      // 创建 docker 容器
-      execSync(
-        `docker run -d -p 8888:80 --name ${data.repository.name}-container ${data.repository.name}-image:latest`,
-        {
-          stdio: "inherit",
-        }
-      );
+    //   // 创建 docker 容器
+    //   execSync(
+    //     `docker run -d -p 8888:80 --name ${data.repository.name}-container ${data.repository.name}-image:latest`,
+    //     {
+    //       stdio: "inherit",
+    //     }
+    //   );
 
-      console.log("deploy success");
-    }
+    //   console.log("deploy success");
+    // }
     res.end("ok");
   })
   .listen(3000, () => {
